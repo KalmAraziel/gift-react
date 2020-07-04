28bcc0048ce527ca02cfb63520f2c90745b65102
Marcos Ahumada <marcos.ahumada@example.com>
2020-07-04 18:00:16 -0400

    docs agregado

---
+++ docs/precache-manifest.2e99559846c36ff853ead98150963832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6856d4012e3288c3777c3f5ef0cf6200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17332b7c51dcde81e71f",
+    "url": "/static/css/main.b45c2e7c.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17332b7c51dcde81e71f",
+    "url": "/static/js/main.0c54a20c.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
